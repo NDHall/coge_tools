bd0dc6662b06c74ae0c9de5b537cfc5965c04d1a
NDHall <ndh0004@auburn.edu>
2018-10-11 09:50:45 -0500

    modified bam2bed. I was passing a string instead of an integer for merged bed. This caused problems with the sorting.

--- viz/bam2bed_mate_merge.py
+++ viz/bam2bed_mate_merge.py
@@ -11,23 +11,26 @@ def main(handle):
     read_merge = {}
     reads = BedTool(handle)
     for read in reads.sort():
-        print(read.chrom)
         key = '/'.join(read[3].split('/')[:-1])
         if key not in read_merge :
             read_merge[key] = [read]
         else :
             read_merge[key].append(read)
+
+    print("""
+    Finshed reading Reads.\n
+    {l} read_matches found""".format(l=len(read_merge)))
+
     for key in read_merge :
         if len(read_merge[key]) == 2 :
             n,u = read_merge[key]
-            print(n[0],u[0])
             if n[0] == u[0]:
-                if n[1] < u[1] and \
-                        n[2] <= u[1]:
+                if n.stop < u.start and \
+                        n.stop < u.stop:
                     p5 = n
                     p3 = u
-                elif  n[1] > u[2] and \
-                        n[1] > u[2]:
+                elif  n.start > u.stop and \
+                        n.start > u.start:
                     p5 = u
                     p3 = n
                 else :
@@ -37,6 +40,14 @@ def main(handle):
                     right now we are excluding all overlapping reads. 
                     """
                 if p5 is not None :
+                    start = p5.start
+                    stop = p3.stop
+                    assert start < stop , 'Malformed bed start: {start} stop:{stop}\n{bed1} : {bed2}'.format(
+                                                                                 start=start,
+                                                                                 stop=stop,
+                                                                                 bed1=str(p5),
+                                                                                 bed2=str(p3))
+
                     outstring = '{c}\t{start}\t{stop}\t{r}\t{score}\t.\n'.format(
                         c=p5[0],
                         start=p5[1],
@@ -54,5 +65,5 @@ def main(handle):
 
 
 if __name__ == '__main__':
-    for x in ['/home/ndh0004/Downloads/tmp_passthrough_files/sort_by_name_MP_raw_DRR095902_Ecor_PR202_scaffoldsv2.bed']:
+    for x in ['/home/ndh0004/code/coge_tools/data_out/MP_looseMapLocalNu30K.bed']:
         main(x)
